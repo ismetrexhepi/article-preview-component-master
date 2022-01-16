fade0aa8a180ab8e277c75681b2dc0175819e1b8
ismetrexhepi <90641870+ismetrexhepi@users.noreply.github.com>
2022-01-16 21:51:17 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let click=document.querySelector(".click");
+
+let share =document.querySelector(".share");
+
+share.addEventListener("click",function(){
+    // alert("clicked")
+
+    click.classList.toggle("active")
+})
+
+
